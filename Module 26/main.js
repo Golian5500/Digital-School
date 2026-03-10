e845a246fc132cc56d574f7d957f3ce5b9c59c6d
Drin Hajzeraj <131485380+Golian5500@users.noreply.github.com>
2026-03-10 17:20:15 +0100

    update

--- Module 26/main.js
+++ Module 26/main.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+var name = "Drin";
+var surname = "Hajzeraj";
+var full_name = name + " " + surname;
+console.log(full_name);
+
+var age = 14;
+
+console.log(age <= 18 && age >= 10); 
+
+
+
+var age1 = 15;
+
+console.log(age1 >= 18 || age1 >=10);
+
+
+var one = 16;
+var two = 16;
+
+console.log(!(one == two));
