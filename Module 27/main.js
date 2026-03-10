2e33e3468b2a96aa50b9e3ddde0b48103c3b4228
Drin Hajzeraj <131485380+Golian5500@users.noreply.github.com>
2026-03-10 17:43:05 +0100

    update

---
+++ Module 27/main.js
@@ -0,0 +1,9 @@
+function displayAlert(){
+    alert("This text got displayed by the calling of the function")
+}
+displayAlert();
+
+function writeHTML(number1, number2) {
+    return number1+number2;
+}
+document.write(writeHTML(8, 3));
