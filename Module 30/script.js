af489319a043fad3459525680f262bb205a1aa3f
Drin Hajzeraj <131485380+Golian5500@users.noreply.github.com>
2026-03-26 17:44:17 +0100

    update

---
+++ Module 30/script.js
@@ -0,0 +1,8 @@
+var mosha = 8;
+if (mosha < 8) {
+    document.write("Your child is to young to be enlisted for this programming course.");
+} else if(mosha > 18) {
+    document.write("Your too old to be enlisted for this programming course.");
+} else {
+    document.write("You have successfully been enrolled your child for this course. We will contact you for further information via Call.");
+}
