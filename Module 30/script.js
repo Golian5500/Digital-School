41e4a5d5ef0346a2958e9d7b3345923ba013a080
Drin Hajzeraj <131485380+Golian5500@users.noreply.github.com>
2026-03-31 16:33:33 +0100

    update

--- Module 30/script.js
+++ Module 30/script.js
@@ -6,3 +6,34 @@ if (mosha < 8) {
 } else {
     document.write("You have successfully been enrolled your child for this course. We will contact you for further information via Call.");
 }
+
+var input = document.getElementById("input_id");
+var button = document.getElementById("btn_id");
+var text = document.getElementById("text_id");
+
+button.onclick = function() {
+    text.innerHTML = input.value;
+}
+
+var num1 = document.getElementById("num1");
+var num2 = document.getElementById("num2");
+var calcBtn = document.getElementById("resultBtn");
+var resultText = document.getElementById("resultText");
+var ch1 = document.getElementById("theMainH1");
+
+function mbledhje(n1, n2) {
+    var barazimi = resultText.innerHTML = Number(n1.value) + Number(n2.value);
+    
+    if (barazimi > 10) {
+        ch1.innerHTML = "Result is greater than 10";
+    } else if (barazimi < 10) {
+        ch1.innerHTML = "Result is less than 10";
+    } else {
+        ch1.innerHTML = "Result is equal to 10";
+    }
+
+}
+
+calcBtn.onclick = function() {
+    mbledhje(num1, num2);
+};
